9afef80d3a2f1de1667b3a7cf21a73e36c3be690
Cleo Aguiar <cleotaisas@gmail.com>
2018-09-22 09:06:17 -0300

    refactor: configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Imports at the bottom avoids the error that results from 
 # the mutual references between these two files
