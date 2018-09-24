a05ee0983cf372651fe43b8ad7780fe92e56b5ce
Cleo Aguiar <cleotaisas@gmail.com>
2018-09-24 18:14:31 -0300

    feat: User Login - Login - Logout - Requiring users to login

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Imports at the bottom avoids the error that results from 
+login = LoginManager(app)
+login.login_view = 'login'
+
+# Imports at the bottom avoids the error that results from
 # the mutual references between these two files
 from app import routes, models
