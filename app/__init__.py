22ca78d9ca25db914a3134ff68174a0aaa1519cf
Cleo Aguiar <cleotaisas@gmail.com>
2018-09-24 13:08:33 -0300

    feat: Database - Relational database - Two table: User and Post - Shell Context
    **
    after updates to the application models:
    $ flask db migrate -m "message"
    $ flask db upgrade
    **

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Imports at the bottom avoids the error that results from 
 # the mutual references between these two files
-from app import routes
+from app import routes, models
