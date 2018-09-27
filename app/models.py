d4ce125b380c73a40f9c8e8d655b50bf7d900976
Cleo Aguiar <cleotaisas@gmail.com>
2018-09-27 18:59:33 -0300

    fix: followed posts

--- app/models.py
+++ app/models.py
@@ -51,10 +51,10 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
-    def followed_post(self):
+    def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.followed_id == self.id)
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
