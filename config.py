1923a52bc7a50b28dc4049430741faa4b7af57f8
Cleo Aguiar <cleotaisas@gmail.com>
2018-10-01 09:21:09 -0300

    feat: Pagination

--- config.py
+++ config.py
@@ -16,3 +16,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mail@gmail.com']
+
+    #Post per page
+    POSTS_PER_PAGE = 3
